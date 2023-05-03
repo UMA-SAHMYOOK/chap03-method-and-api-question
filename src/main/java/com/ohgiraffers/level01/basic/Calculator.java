69accb02725eb6c868b4e67fd233ba9e62fb015a
AzureAD\양한진 <gkswls3203@naver.com>
2023-05-04 00:02:27 +0900

    [basic] [문제'1'] 미완 메소드 코딩 숙련도 부족

---
+++ src/main/java/com/ohgiraffers/level01/basic/Calculator.java
@@ -0,0 +1,26 @@
+package com.ohgiraffers.level01.basic;
+
+public class Calculator {
+    public void checkMethod(){
+        System.out.println("메소드 호출 확인");
+
+
+    }
+    public int sum1to10(){
+        System.out.println("1부터 10까지의 합");
+    }
+    public void checkMaxNumber(10, 20){
+        System.out.println("두 수 중 큰 수는 이다");
+
+    }
+
+    public int sumTwoNumver(10, 20){
+        System.out.println("10과 20의 합");
+
+    }
+
+    public int minusTwoNumber(){
+        System.out.println("10과 5의 차는");
+
+    }
+}
