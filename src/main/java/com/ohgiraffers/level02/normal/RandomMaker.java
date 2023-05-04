ddab5944639414a416a0b491565c4fa4211db8ea
CHOI <seongyong335@gmail.com>
2023-05-04 10:55:39 +0900

    [normal][문제1] 완료
    Application.java
    모든 메소드를 System.out.println으로 호출받아 출력했습니다.
    
    RandomMaker.java
    randomNumber는 최소값과 최대값 사이의 난수를 Math.random()을 통해 구했으며 정수형으로 표시하기 위하여 int로 강제형변환하여 반환하도록 했습니다.
    randomUpperAlphabet은 길이 만큼 반복문으로 반복시키고 대문자 알파벳의 아스키코드값 사이로 난수를 지정하여 해당 난수를 char로 강제형변환 후 공란인 문자열 result에 저장하고 반환하도록 하였습니다.
    rockPaperScissors는 0~3까지의 수를 난수로 받아 0~1사이면 바위 1~2사이면 가위 그 외는 보를 반환하도록 하였습니다.
    tosscoin은 1~10까지 수를 난수로 받아 2로 나누고 나머지가 0이면 앞면 1이면 뒷면으로 반환하도록 하였습니다.

--- src/main/java/com/ohgiraffers/level02/normal/RandomMaker.java
+++ src/main/java/com/ohgiraffers/level02/normal/RandomMaker.java
@@ -2,11 +2,11 @@ package com.ohgiraffers.level02.normal;
 
 public class RandomMaker {
 
-    public int randomNumber(int min, int max){
+    public static int randomNumber(int min, int max){
         return ((int)(Math.random() * (max-min+1)) + min);
     }
 
-    public String randomUpperAlphabet(int length){
+    public static String randomUpperAlphabet(int length){
         String result = "";
         for(int i = 0; i < length; i++){
             result += (char)((Math.random() * (90 - 65 + 1)) + 65);
@@ -14,14 +14,14 @@ public class RandomMaker {
         return result;
     }
 
-    public String rockPaperScissors(){
+    public static String rockPaperScissors(){
         double tmp = (Math.random() * 3);
         if(tmp >= 0 && tmp < 1) return "바위";
         else if (tmp >= 1 && tmp < 2) return "가위";
         else return "보";
     }
 
-    public String tossCoin(){
+    public static String tossCoin(){
         int coin = (int)((Math.random() * 10) + 1);
         if(coin % 2 == 0) return "앞면";
         else return "뒷면";
